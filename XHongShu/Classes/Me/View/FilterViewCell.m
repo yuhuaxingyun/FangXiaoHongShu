88649be40f12dad5d49be3ae0ae498671cab29cc
yuhuaxingyun <soukou@wanting.local>
2016-09-26 10:29:55 +0800

    xiaohongshu
    第一版

---
+++ XHongShu/Classes/Me/View/FilterViewCell.m
@@ -0,0 +1,34 @@
+//
+//  FilterViewCell.m
+//  XHongShu
+//
+//  Created by 宋江 on 16/7/1.
+//  Copyright © 2016年 MuChen. All rights reserved.
+//
+
+#import "FilterViewCell.h"
+#import "Auxiliary.h"
+@implementation FilterViewCell
+- (instancetype)initWithFrame:(CGRect)frame{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self createUI];
+    }
+    return self;
+}
+- (void)createUI{
+    
+    _imageView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0,90, 90)];
+    [self.contentView addSubview:_imageView];
+    
+    _nameLabel = [[UILabel alloc]initWithFrame:CGRectMake(0, 90, 90, 30)];
+    _nameLabel.font = [UIFont systemFontOfSize:12];
+    _nameLabel.textColor = [UIColor grayColor];
+    _nameLabel.textAlignment = NSTextAlignmentCenter;
+    [self.contentView addSubview:_nameLabel];
+}
+- (void)setMuDict:(NSMutableDictionary *)muDict{
+    _muDict = muDict;
+    self.imageView.image = [Auxiliary changeImage:[[[_muDict allKeys] lastObject] intValue] imageView:[[_muDict allValues] lastObject]];
+}
+@end
